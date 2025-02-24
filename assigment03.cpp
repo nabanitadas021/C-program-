06f9c911c73750f7cb855f5d1edc0057226cf578
nabanitadas021 <dasn62779@gmail.com>
2025-02-24 15:21:35 +0530

    Add files via upload

---
+++ assigment03.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,b;
+    scanf("%d%d",&a,&b);
+	printf("%d%d%d",a,b,a*b);
+	scanf("%d%d",&a,&b);
+	printf("%d%d%d",a,b,a*b);
+	scanf("%d%d",&a,&b);
+	printf("%d%d%d",a,b,a*b);
+	scanf("%d%d",&a,&b);
+	printf("%d%d%d",a,b,a*b); 
+	scanf("%d%d",&a,&b);
+	printf("%d%d%d",a,b,a*b); 
+	return 0;
+}
